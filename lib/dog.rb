bfcf0e0ba3ae523322b68e106526d004da6be3f5
bdb2381 <brad.beggs@gmail.com>
2020-07-14 15:51:14 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,32 @@
+require 'pry'
+
+class Dog
+  
+  
+    def name=(name)
+        @name = name
+        @name = name
+        @breed = breed
+        binding.pry
+       
+    def name
+        @name
+    end 
+
+    end 
+
+    def breed
+        @breed
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end 
+
+
+
+  
+
+
+
+end 
